7177dac59813ec6996844dba4df74095e18132c4
Mohd.Arbaz <mdarbaz1711@gmail.com>
2025-04-11 22:19:39 +0530

    fix: added vite and updated build script

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL + '/api',
+
 });
 
 instance.interceptors.request.use((config) => {
